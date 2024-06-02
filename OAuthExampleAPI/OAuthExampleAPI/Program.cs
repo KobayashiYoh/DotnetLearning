3061b60024cd410e931d6289c365ea31dd729caf
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-06-02 14:59:51 +0900

    WeatherForecastレコードを削除

--- OAuthExampleAPI/OAuthExampleAPI/Program.cs
+++ OAuthExampleAPI/OAuthExampleAPI/Program.cs
@@ -48,8 +48,3 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
